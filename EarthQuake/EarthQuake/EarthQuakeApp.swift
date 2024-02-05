1a46b41a30acf08c193dc48991e9da876387613e
sher304 <esherovshermat@gmail.com>
2024-02-06 00:26:58 +0100

    Create project

---
+++ EarthQuake/EarthQuake/EarthQuakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakeApp.swift
+//  EarthQuake
+//
+//  Created by Шермат Эшеров on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
